8dd61fde12894f5c00d534b31a56896cac2ac783
zak-off-zak <zak.von.zak@gmail.com>
2025-12-03 15:52:56 +0100

    Discovery and Ack packet structure

--- include/Packet.hpp
+++ include/Packet.hpp
@@ -3,12 +3,12 @@
 
 #include <cstdint>
 #include <cstring>
+#include "config.hpp"
 
 enum class PacketType : uint8_t {
   DIS = 0x01,
   ACK = 0x02,
   DAT = 0x03,
-  KAL = 0x04,
 };
 
 #pragma pack(push, 1)
@@ -18,10 +18,23 @@ struct Packet {
   PacketType type;
   uint8_t ttl;
   uint16_t pkt_id;
-  uint8_t msg[20];
+  uint8_t msg[DATA_PACKET_SIZE];
   uint8_t chs;
 };
 #pragma pack(pop)
+
+struct DiscoveryPacket {
+  uint16_t src;
+  uint16_t dest;
+  PacketType type;
+};
+
+struct AcknowledgePacket {
+  uint16_t src;
+  uint16_t dest;
+  PacketType type;
+  uint16_t pkt_id;
+};
 
 uint8_t checksum(const Packet& pkt);
 size_t serialize(const Packet& pkt, uint8_t* buffer);
---
+++ include/config.hpp
@@ -0,0 +1,6 @@
+#ifndef CONFIG
+#define CONFIG
+
+#define DATA_PACKET_SIZE 20
+
+#endif
