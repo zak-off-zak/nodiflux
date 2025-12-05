3c9475e649843b41b8caf52ee23f686fb4520de9
zak-off-zak <zak.von.zak@gmail.com>
2025-12-05 21:33:28 +0100

    Discovery packet receive

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,20 @@
 #include <esp_now.h>
 
 
-uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
-esp_now_peer_info_t peerInfo;
-
-void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status){
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+void OnDataReceive(const uint8_t* mac_addr, const uint8_t *incomingData, int len){
+  DiscoveryPacket data;
+  memcpy(&data, incomingData, sizeof(DiscoveryPacket));
+  Serial.print("LEN: ");
+  Serial.println(len);
+  Serial.print("src: ");
+  Serial.println(data.src);
+  // Serial.print("type: ");
+  // Serial.println(data.type);
+  Serial.print("pkt_id: ");
+  Serial.println(data.pkt_id);
+  Serial.print("chs: ");
+  Serial.println(data.chs);
+  Serial.println();
 }
 
 void setup() {
@@ -23,32 +30,8 @@ void setup() {
     return; // TODO: Error handling or retry
   }
 
-  esp_now_register_send_cb(OnDataSent);
-
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 1;
-  peerInfo.encrypt = false;
-
-  if(esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
-  }
+  esp_now_register_recv_cb(OnDataReceive);
 }
 
 void loop() {
-  DiscoveryPacket dsk_pkt{};
-  WiFi.macAddress().toCharArray(dsk_pkt.src, sizeof(dsk_pkt.src));
-  dsk_pkt.type = PacketType::DIS;
-  dsk_pkt.pkt_id = random(1,20); 
-  dsk_pkt.chs = checksum(dsk_pkt);
-
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &dsk_pkt, sizeof(dsk_pkt));
-
-  if (result == ESP_OK) {
-    Serial.println("Sending confirmed");
-  }
-  else {
-    Serial.println("Sending error");
-  }
-  delay(2000);
 }
