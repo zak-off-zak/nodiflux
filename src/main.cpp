3f2a29ae4c84b499d7d664aed0a201d5b38a22c0
zak-off-zak <zak.von.zak@gmail.com>
2025-12-16 17:45:28 +0100

    Assignining tasks to different cores

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <cstdint>
 #include <esp_now.h>
 #include "Protocol.hpp"
 #include "config.hpp"
+#include "freertos/portmacro.h"
 
 uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-const uint8_t mac_addr[] = {0x20, 0xE7, 0xC8, 0x59, 0x57, 0x64};
+const uint8_t mac_addr[] = {0x20, 0xE7, 0xC8, 0x59, 0xE9, 0x18};
+
+void discoveryTask(void *param){
+  uint8_t* addr = (uint8_t*) param;
+  while(true){
+    SendDiscoveryPacket(addr);
+    vTaskDelay(DIS_BROADCAST_SPEED / portTICK_PERIOD_MS);
+  }
+}
+
+void dataTask(void *param){
+  uint8_t* addr = (uint8_t*) param;
+  while(true){
+    SendDataPacket(addr);
+    vTaskDelay(DIS_BROADCAST_SPEED / portTICK_PERIOD_MS);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -20,10 +38,14 @@ void setup() {
   // esp_now_register_send_cb(OnDataSent);
 
   establishPeer(broadcastAddress, PEER_CHANNEL, PEER_ENCRYPT);
+
+  xTaskCreatePinnedToCore(discoveryTask, "discoveryTask", 4096, (void*)broadcastAddress, 1, NULL, 0);
+  xTaskCreatePinnedToCore(dataTask, "dataTask", 4096, (void*)mac_addr, 1, NULL, 1);
 }
 
+
 void loop() {
-  SendDiscoveryPacket(broadcastAddress);
+  // SendDiscoveryPacket(broadcastAddress);
   // SendDataPacket(mac_addr);
   // NodeRegistry::instance().debug();
   // NodeRegistry::instance().debugMostRecentNode();
