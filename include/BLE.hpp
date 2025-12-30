94db81dc631a09ed5d56593b94bdf30bd2f117e4
zak-off-zak <zak.von.zak@gmail.com>
2025-12-30 15:02:39 +0100

    BLE docs

--- include/BLE.hpp
+++ include/BLE.hpp
@@ -9,25 +9,67 @@
 
 void initBLE();
 
+/**
+ * @class BLEController
+ * @brief The class used to createa singelton for BLE communication
+ *
+ */
 class BLEController {
   private:
     NimBLECharacteristic* tx = nullptr;
     NimBLECharacteristic* rx = nullptr;
   public:
+    /**
+     * @brief Default constructor
+     */
     BLEController () = default;
+    /**
+     * @brief Delete the copy constructor
+     */
     BLEController(const BLEController&) = delete;
+    /**
+     * @brief Detele copy assignment operator
+     *
+     * @return [TODO:return]
+     */
     BLEController& operator=(const BLEController&) = delete;
 
+    /**
+     * @brief Method to get the singelton instance
+     *
+     * @return [TODO:return]
+     */
     static BLEController& instance() {
       static BLEController instance;
       return instance;
     }
 
+    /**
+     * @brief Initilialize the BLE
+     */
     void init();
+
+    /**
+     * @brief Trasmit a message using BLE
+     *
+     * @param msg Message to tarnsmit
+     * @return True, if trasmission is successful
+     */
     bool transmit(const std::string& msg);
 };
 
+/**
+ * @class RxCallbacks
+ * @brief A class to define characteristics call backs
+ *
+ */
 class RxCallbacks : public NimBLECharacteristicCallbacks{
+  /**
+   * @brief A callback to be triggered once a message is recieved
+   *
+   * @param ch characteristic to be monitored
+   * @param connInfo Connection Information
+   */
   void onWrite(NimBLECharacteristic* ch, NimBLEConnInfo& connInfo) override;
 };
 
