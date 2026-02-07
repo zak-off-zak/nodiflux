c3b02fed5a83512e307a833517b4362d9c46857c
zak-off-zak <zak.von.zak@gmail.com>
2026-02-07 17:18:49 +0100

    Implementation of the Bloom Filter methods and testing

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -6,9 +6,17 @@
 
 void BloomFilter::addEntry(uint16_t pkt_id) {
   std::lock_guard<std::mutex> lock(this->mtx);
+  this->bloom.set(this->h1(pkt_id) % this->bloom.size());
+  this->bloom.set(this->h2(pkt_id) % this->bloom.size());
+  this->bloom.set(this->h3(pkt_id) % this->bloom.size());
 }
 
-bool BloomFilter::isSeen(uint16_t pkt_id) { return false; }
+bool BloomFilter::isSeen(uint16_t pkt_id) {
+  std::lock_guard<std::mutex> lock(this->mtx);
+  return this->bloom.test(this->h1(pkt_id) % this->bloom.size()) &&
+         this->bloom.test(this->h2(pkt_id) % this->bloom.size()) &&
+         this->bloom.test(this->h3(pkt_id) % this->bloom.size());
+}
 
 uint8_t BloomFilter::mix1(uint8_t message_block, uint8_t internal_state) {
   return ((message_block << 4) * (internal_state >> 3));
@@ -17,8 +25,8 @@ uint8_t BloomFilter::mix1(uint8_t message_block, uint8_t internal_state) {
 uint16_t BloomFilter::h1(const uint16_t data) {
   uint8_t internal_state = 0xA5;
 
-  const uint8_t *bytes = reinterpret_cast<const uint8_t *>(data);
-  for (size_t i = 0; i < sizeof(bytes); i++) {
+  const uint8_t *bytes = reinterpret_cast<const uint8_t *>(&data);
+  for (size_t i = 0; i < sizeof(data); i++) {
     internal_state = this->mix1(bytes[i], internal_state);
   }
 
@@ -29,8 +37,8 @@ uint16_t BloomFilter::h1(const uint16_t data) {
 uint16_t BloomFilter::h2(const uint16_t data) {
   uint8_t internal_state = 0xA6;
 
-  const uint8_t *bytes = reinterpret_cast<const uint8_t *>(data);
-  for (size_t i = 0; i < sizeof(bytes); i++) {
+  const uint8_t *bytes = reinterpret_cast<const uint8_t *>(&data);
+  for (size_t i = 0; i < sizeof(data); i++) {
     internal_state = this->pearson_permutations[internal_state % bytes[i]];
   }
 
@@ -42,8 +50,8 @@ uint16_t BloomFilter::h2(const uint16_t data) {
 uint16_t BloomFilter::h3(const uint16_t data) {
   uint8_t internal_state = 0xA7;
 
-  const uint8_t *bytes = reinterpret_cast<const uint8_t *>(data);
-  for (size_t i = 0; i < sizeof(bytes); i++) {
+  const uint8_t *bytes = reinterpret_cast<const uint8_t *>(&data);
+  for (size_t i = 0; i < sizeof(data); i++) {
     internal_state =
         this->crc_8_autostar_lookup_table
             [internal_state % sizeof(this->crc_8_autostar_lookup_table)] *
--- test/test_bloom_filter.cpp
+++ test/test_bloom_filter.cpp
@@ -1,5 +1,85 @@
-#include "BloomFilter.hpp"
+#include "../src/BloomFilter.cpp"
 #include "gtest/gtest.h"
 #include <cstdint>
+#include <functional>
+#include <iostream>
+#include <thread>
+#include <vector>
 
-TEST(TestBloomFilter, AddEntry) { EXPECT_EQ(10, 10); }
+TEST(TestBloomFilter, IsSeen) {
+  BloomFilter &bf = BloomFilter::instance();
+
+  uint16_t pkt_id = 0xAAAA;
+
+  EXPECT_FALSE(bf.isSeen(pkt_id));
+
+  bf.addEntry(pkt_id);
+
+  EXPECT_TRUE(bf.isSeen(pkt_id));
+}
+
+TEST(TestBloomFilter, MultipleEntries) {
+  BloomFilter &bf = BloomFilter::instance();
+
+  uint16_t pkt_id1 = 0x1235;
+  uint16_t pkt_id2 = 0xABCD;
+  uint16_t pkt_id3 = 0xA5A5;
+
+  EXPECT_FALSE(bf.isSeen(pkt_id1));
+  EXPECT_FALSE(bf.isSeen(pkt_id2));
+  EXPECT_FALSE(bf.isSeen(pkt_id3));
+
+  bf.addEntry(pkt_id1);
+  bf.addEntry(pkt_id2);
+  bf.addEntry(pkt_id3);
+
+  EXPECT_TRUE(bf.isSeen(pkt_id1));
+  EXPECT_TRUE(bf.isSeen(pkt_id2));
+  EXPECT_TRUE(bf.isSeen(pkt_id3));
+}
+
+TEST(TestBloomFilter, RepeatedEntry) {
+  BloomFilter &bf = BloomFilter::instance();
+
+  uint16_t pkt_id = 0x123A;
+
+  EXPECT_FALSE(bf.isSeen(pkt_id));
+
+  bf.addEntry(pkt_id);
+  bf.addEntry(pkt_id);
+
+  EXPECT_TRUE(bf.isSeen(pkt_id));
+}
+
+TEST(TestBloomFilter, HashCollisions) {
+  BloomFilter &bf = BloomFilter::instance();
+
+  for (uint16_t i = 0; i < 50; i++) {
+    bf.addEntry(i);
+  }
+
+  for (uint16_t i = 0; i < 50; i++) {
+    EXPECT_TRUE(bf.isSeen(i));
+  }
+}
+
+TEST(TestBloomFilter, ThreadSafety) {
+  BloomFilter &bf = BloomFilter::instance();
+
+  std::function<void(uint16_t)> addEntry_fn = [&bf](uint16_t id) {
+    bf.addEntry(id);
+  };
+
+  std::vector<std::thread> threads;
+  for (uint16_t i = 200; i < 250; i++) {
+    threads.emplace_back(addEntry_fn, i);
+  }
+
+  for (std::thread &t : threads) {
+    t.join();
+  }
+
+  for (uint16_t i = 200; i < 250; i++) {
+    EXPECT_TRUE(bf.isSeen(i));
+  }
+}
