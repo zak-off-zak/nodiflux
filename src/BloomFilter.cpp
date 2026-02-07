2456a913421155c1cf58b3619a7c049876e4e3cd
zak-off-zak <zak.von.zak@gmail.com>
2026-02-07 14:43:17 +0100

    Hash functions implemented

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -1,4 +1,7 @@
 #include "BloomFilter.hpp"
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
 #include <mutex>
 
 void BloomFilter::addEntry(uint16_t pkt_id) {
@@ -6,3 +9,49 @@ void BloomFilter::addEntry(uint16_t pkt_id) {
 }
 
 bool BloomFilter::isSeen(uint16_t pkt_id) { return false; }
+
+uint8_t BloomFilter::mix1(uint8_t message_block, uint8_t internal_state) {
+  return ((message_block << 4) * (internal_state >> 3));
+}
+
+uint16_t BloomFilter::h1(const uint16_t data) {
+  uint8_t internal_state = 0xA5;
+
+  const uint8_t *bytes = reinterpret_cast<const uint8_t *>(data);
+  for (size_t i = 0; i < sizeof(bytes); i++) {
+    internal_state = this->mix1(bytes[i], internal_state);
+  }
+
+  return (static_cast<uint16_t>(internal_state) << 8) |
+         (static_cast<uint16_t>(this->mix1(internal_state, internal_state)));
+}
+
+uint16_t BloomFilter::h2(const uint16_t data) {
+  uint8_t internal_state = 0xA6;
+
+  const uint8_t *bytes = reinterpret_cast<const uint8_t *>(data);
+  for (size_t i = 0; i < sizeof(bytes); i++) {
+    internal_state = this->pearson_permutations[internal_state % bytes[i]];
+  }
+
+  return (static_cast<uint16_t>(internal_state) << 8) |
+         (static_cast<uint16_t>(
+             this->pearson_permutations[internal_state % 0xA6]));
+}
+
+uint16_t BloomFilter::h3(const uint16_t data) {
+  uint8_t internal_state = 0xA7;
+
+  const uint8_t *bytes = reinterpret_cast<const uint8_t *>(data);
+  for (size_t i = 0; i < sizeof(bytes); i++) {
+    internal_state =
+        this->crc_8_autostar_lookup_table
+            [internal_state % sizeof(this->crc_8_autostar_lookup_table)] *
+        internal_state;
+  }
+
+  return (static_cast<uint16_t>(internal_state) << 8) |
+         (static_cast<uint16_t>(
+             this->crc_8_autostar_lookup_table
+                 [internal_state % sizeof(this->crc_8_autostar_lookup_table)]));
+}
---
+++ test/test_bloom_filter.cpp
@@ -0,0 +1,5 @@
+#include "BloomFilter.hpp"
+#include "gtest/gtest.h"
+#include <cstdint>
+
+TEST(TestBloomFilter, AddEntry) { EXPECT_EQ(10, 10); }
